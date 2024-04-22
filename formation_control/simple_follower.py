1515453d66f4ba171407c7a7f5f82869c76f9f93
Mike Xiangyu Liu <liu.xiangyu@ucy.ac.cy>
2024-04-22 14:37:42 +0300

    add vel limit

--- formation_control/simple_follower.py
+++ formation_control/simple_follower.py
@@ -4,11 +4,12 @@ from simple_pid import PID
 
 
 class SimpleFollower:
-    def __init__(self, x_self_target, y_self_target):
+    def __init__(self, x_self_target, y_self_target, vel_limit):
         self.x_pid = PID(0.5, 0.1, 0.01, setpoint=0)
         self.y_pid = PID(0.5, 0.1, 0.01, setpoint=0)
         self.x_self_target = x_self_target
         self.y_self_target = y_self_target
+        self.vel_limit = vel_limit
 
     def follow(self, pos_self_target):
         x_error = self.x_self_target - pos_self_target[0]
@@ -16,5 +17,10 @@ class SimpleFollower:
 
         x_control = self.x_pid(x_error)
         y_control = self.y_pid(y_error)
+
+        if np.abs(x_control) > self.vel_limit[0]:
+            x_control = np.sign(x_control) * self.vel_limit[0]
+        if np.abs(y_control) > self.vel_limit[1]:
+            y_control = np.sign(y_control) * self.vel_limit[1]
 
         return x_control, y_control
